6283122790797d295f770ba77833c418ef584987
1833994837 <124540729+1833994837@users.noreply.github.com>
2023-04-22 10:31:32 +0800

    Update client.js

--- client.js
+++ client.js
@@ -45,8 +45,8 @@
       <div class="index">${index + 1}</div>
       <div class="name">${item.name}</div>
       <div class="more">
-        <img class="remove" src="./remove.svg" alt="移出播放列表" title="移出播放列表" />
-        <img class="add" src="./check.svg" alt="加入播放列表" title="加入播放列表" />
+        <img class="remove" src="./remove.svg"  title="移出播放列表" />
+        <img class="add" src="./check.svg"  title="加入播放列表" />
       </div>
     </div>`;
   }
